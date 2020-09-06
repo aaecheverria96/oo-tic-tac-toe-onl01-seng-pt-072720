ffe711e1d6e2565106f7f993c84c318dd08df49c
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-06 00:52:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -129,8 +129,8 @@ def play
     turn
   end
   if won?
-    puts "Congratulations #{winner}!" 
-  elsif draw? 
+    puts "Congratulations #{winner}!"
+  elsif draw?
     puts "Cat's Game!"
   end
 end
